0d75cf7111f472658261f1acbba94dbbf9d68067
vishalkr-automation <vishalkr.automation@gmail.com>
2022-11-29 23:12:48 +0530

    My Initial commit

---
+++ src/main/java/CucumberProjectByCodeStudio/CucumberProjectByCodeStudio/App.java
@@ -0,0 +1,13 @@
+package CucumberProjectByCodeStudio.CucumberProjectByCodeStudio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
